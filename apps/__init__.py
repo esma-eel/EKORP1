b9d15ac4f9e5720e275f9b9ec4e3d8a09856b5d8
Esmaeel <esmaeelkomijani@gmail.com>
2024-12-20 16:07:56 +0330

    just testing

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,12 +3,13 @@ from importlib import import_module
 
 from flask import Flask
 
-from config import Config
+from config import get_config
 
 
 def register_blueprints(app):
     blueprint_directories = [
-        directory for directory in os.listdir(os.path.dirname(__file__))
+        directory
+        for directory in os.listdir(os.path.dirname(__file__))
         if os.path.isdir(os.path.join(os.path.dirname(__file__), directory))
         and not directory.startswith("_")
     ]
@@ -17,11 +18,16 @@ def register_blueprints(app):
         module = import_module(f"apps.{blueprint}")
         if hasattr(module, blueprint):
             app.register_blueprint(getattr(module, blueprint))
-        
 
-def create_app(config_class=Config):
-    app = Flask(__name__, template_folder="_templates", static_folder="_statics")
+
+def create_app(config_class=None):
+    if not config_class:
+        config_class = get_config()
+    app = Flask(
+        __name__, template_folder="_templates", static_folder="_statics"
+    )
     app.config.from_object(config_class)
+    print(app.config["PROD_DATABASE_URL"])
     register_blueprints(app)
 
     return app
--- config.py
+++ config.py
@@ -2,7 +2,49 @@ import os
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(BASE_DIR, 'envs/.config.env'))
 
-class Config:
+
+class BaseConfig:
+    @classmethod
+    def load_env_files(cls):
+        env_dir = os.path.join(BASE_DIR, "envs")
+
+        main_env = os.path.join(env_dir, ".env")
+        if os.path.exists(main_env):
+            load_dotenv(main_env)
+
+        current_env = os.getenv("FLASK_ENV", "production")
+
+        env_files = [
+            ".secrets.env",  # secrets
+            f".{current_env}.env",  # environment setting
+            ".local.env",  # local settings
+        ]
+
+        for env_file in env_files:
+            env_file_path = os.path.join(env_dir, env_file)
+            if os.path.exists(env_file_path):
+                load_dotenv(env_file_path, override=True)
+                print(f"Loaded {env_file}")
+            else:
+                print(f"File not found: {env_file}")
+
+    @classmethod
+    def init_config(cls):
+        cls.load_env_files()
+
+
+class DevelopmentConfig(BaseConfig):
     pass
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+def get_config():
+    BaseConfig.init_config()
+
+    env = os.getenv("FLASK_ENV", "production")
+    if env == "production":
+        return ProductionConfig
+    return DevelopmentConfig
