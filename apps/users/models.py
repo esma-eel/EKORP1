5235e196583cebcbf833f0fb5e1f25dea1f9b2dd
Esmaeel <esmaeelkomijani@gmail.com>
2025-03-20 16:57:10 +0330

    added profiles blueprint and model and relation to user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -10,8 +10,14 @@ from apps.extensions import db
 from apps.mixins.timestamps import TimestampMixin
 
 
+# to prevent circular import only import when type hinting
+if TYPE_CHECKING:
+    from profiles.models import UserProfile
+
+
 class User(TimestampMixin, UserMixin, db.Model):
     __tablename__ = "users"
+    # user fields
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(
         sa.String(64), index=True, unique=True
@@ -20,6 +26,12 @@ class User(TimestampMixin, UserMixin, db.Model):
         sa.String(120), index=True, unique=True
     )
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    # foreign keys
+    # 1:1
+    profile: so.Mapped["UserProfile"] = db.relationship(
+        back_populates="user", uselist=False
+    )
 
     def __repr__(self):
         return f"<User {self.username}>"
